219cb13543f70a51b6557af3d9df4f63a7c4b94c
Gigi M <mmgamao1@gmail.com>
2019-05-02 15:17:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function shout(string){
-  return String.toUpperCase()
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  return string.toUpperCase;
 }
 
--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,7 @@
+describe('logWhisper(string)', function ()
+ it('receives one argument and returns it in all lowercase', function() {
+expect(shout('hello')).toEqual('HELLO') ;
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function()   {
-    expect(shout('hello')).toEqual('HELLO');
-  });
 });
 
 describe('whisper(string)', function() {
